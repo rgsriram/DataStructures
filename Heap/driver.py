9abb0e4416a812fee397c076d462849dffa3c7fa
Sriram Ganesh <sriram@belong.co>
2017-11-02 08:32:52 +0530

    reformatting

--- Heap/driver.py
+++ Heap/driver.py
@@ -3,41 +3,39 @@ import heapq
 
 
 def build_min_heap(elements):
-	min_heap = MinHeap()
+    min_heap = MinHeap()
 
-	i = len(elements) // 2
-	min_heap.current_size = len(elements)
-	min_heap.heap_list = [0] + elements[:]
+    i = len(elements) // 2
+    min_heap.current_size = len(elements)
+    min_heap.heap_list = [0] + elements[:]
 
-	while i > 0:
-		min_heap.perc_down(i)
-		i -= 1
+    while i > 0:
+        min_heap.perc_down(i)
+        i -= 1
 
-	print min_heap.heap_list[1:]
-
+    print min_heap.heap_list[1:]
 
 
 def build_max_heap(elements):
-	max_heap = MaxHeap()
+    max_heap = MaxHeap()
 
-	i = len(elements) // 2
-	max_heap.current_size = len(elements)
-	max_heap.heap_list = [0] + elements[:]
+    i = len(elements) // 2
+    max_heap.current_size = len(elements)
+    max_heap.heap_list = [0] + elements[:]
 
-	while i > 0:
-		max_heap.perc_down(i)
-		i -= 1
+    while i > 0:
+        max_heap.perc_down(i)
+        i -= 1
 
-	print max_heap.heap_list[1:]
+    print max_heap.heap_list[1:]
 
 
 if __name__ == '__main__':
-	li = [0, 9, 5, 6, 2, 3]
+    li = [0, 9, 5, 6, 2, 3]
 
-	build_min_heap(li)
-	build_max_heap(li)
+    build_min_heap(li)
+    build_max_heap(li)
 
-	# heapq.heapify(li)
-	heapq._heapify_max(li)
-	print li
-
+    # heapq.heapify(li)
+    heapq._heapify_max(li)
+    print li
--- Heap/heaps.py
+++ Heap/heaps.py
@@ -1,117 +1,105 @@
 class MinHeap(object):
-	def __init__(self):
-		self.heap_list = []
-		self.current_size = 0
+    def __init__(self):
+        self.heap_list = []
+        self.current_size = 0
 
+    def perc_up(self, i):
+        """
+        Move the leaf node is lesser than root, move it up.
+        """
 
-	def perc_up(self, i):
-		"""
-		Move the leaf node is lesser than root, move it up.
-		"""
-		while (i // 2) > 0:
-			if self.heap_list[i] < self.heap_list[i//2]:
-				self.heap_list[i], self.heap_list[i//2] = self.heap_list[i//2], self.heap_list[i]
+        while (i // 2) > 0:
+            if self.heap_list[i] < self.heap_list[i // 2]:
+                self.heap_list[i], self.heap_list[i // 2] = self.heap_list[i // 2], self.heap_list[i]
 
-			i //= 2
+            i //= 2
 
+    def insert(self, element):
+        self.heap_list.append(element)
+        self.current_size += 1
+        self.perc_up(self.current_size)
 
+    def perc_down(self, i):
+        """
+        Swap the root node, till achieving heap property
+        """
+        while (i * 2) <= self.current_size:
+            min_pos = self._min(i)
 
-	def insert(self, element):
-		self.heap_list.append(element)
-		self.current_size += 1
-		self._perc_up(self.current_size)
+            if self.heap_list[i] > self.heap_list[min_pos]:
+                self.heap_list[i], self.heap_list[min_pos] = self.heap_list[min_pos], self.heap_list[i]
 
+            i = min_pos
 
-	def perc_down(self, i):
-		"""
-		Swap the root node, till acheiving heap property
-		"""
-		while (i*2) <= self.current_size:
-			min_pos = self._min(i)
+    def _min(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2
 
-			if self.heap_list[i] > self.heap_list[min_pos]:
-				self.heap_list[i], self.heap_list[min_pos] = self.heap_list[min_pos], self.heap_list[i]
+        else:
+            if self.heap_list[i * 2] < self.heap_list[i * 2 + 1]:
+                return i * 2
 
-			i = min_pos			
+            else:
+                return i * 2 + 1
 
+    def del_min(self):
+        element = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size -= 1
+        self.heap_list.pop()
+        self.perc_down(1)
 
-	def _min(self, i):
-		if i * 2 + 1 > self.current_size:
-			return i * 2
-
-		else:
-			if self.heap_list[i*2] < self.heap_list[i*2 + 1]:
-				return i * 2
-
-			else:
-				return i * 2 + 1
-
-
-	def del_min(self):
-		element = self.heap_list[1]
-		self.heap_list[1] = self.heap_list[self.current_size]
-		self.current_size -= 1
-		self.heap_list.pop()
-		self._perc_down(1)
-
-		return element
+        return element
 
 
 class MaxHeap(object):
-	def __init__(self):
-		self.heap_list = []
-		self.current_size = 0
+    def __init__(self):
+        self.heap_list = []
+        self.current_size = 0
 
+    def perc_up(self, i):
+        """
+        Move the leaf node is higher than root, move it up.
+        """
+        while (i // 2) > 0:
+            if self.heap_list[i] < self.heap_list[i // 2]:
+                self.heap_list[i], self.heap_list[i // 2] = self.heap_list[i // 2], self.heap_list[i]
 
-	def perc_up(self, i):
-		"""
-		Move the leaf node is lesser than root, move it up.
-		"""
-		while (i // 2) > 0:
-			if self.heap_list[i] < self.heap_list[i//2]:
-				self.heap_list[i], self.heap_list[i//2] = self.heap_list[i//2], self.heap_list[i]
+            i //= 2
 
-			i //= 2
+    def insert(self, element):
+        self.heap_list.append(element)
+        self.current_size += 1
+        self.perc_up(self.current_size)
 
+    def perc_down(self, i):
+        """
+        Swap the root node, till achieving heap property
+        """
+        while (i * 2) <= self.current_size:
+            max_pos = self._max(i)
 
+            if self.heap_list[i] < self.heap_list[max_pos]:
+                self.heap_list[i], self.heap_list[max_pos] = self.heap_list[max_pos], self.heap_list[i]
 
-	def insert(self, element):
-		self.heap_list.append(element)
-		self.current_size += 1
-		self._perc_up(self.current_size)
+            i = max_pos
 
+    def _max(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2
 
-	def perc_down(self, i):
-		"""
-		Swap the root node, till acheiving heap property
-		"""
-		while (i*2) <= self.current_size:
-			max_pos = self._max(i)
+        else:
+            if self.heap_list[i * 2] > self.heap_list[i * 2 + 1]:
+                return i * 2
 
-			if self.heap_list[i] < self.heap_list[max_pos]:
-				self.heap_list[i], self.heap_list[max_pos] = self.heap_list[max_pos], self.heap_list[i]
+            else:
+                return i * 2 + 1
 
-			i = max_pos			
+    def del_max(self):
+        element = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size -= 1
+        self.heap_list.pop()
+        self.perc_down(1)
 
-
-	def _max(self, i):
-		if i * 2 + 1 > self.current_size:
-			return i * 2
-
-		else:
-			if self.heap_list[i*2] > self.heap_list[i*2 + 1]:
-				return i * 2
-
-			else:
-				return i * 2 + 1
-
-
-	def del_max(self):
-		element = self.heap_list[1]
-		self.heap_list[1] = self.heap_list[self.current_size]
-		self.current_size -= 1
-		self.heap_list.pop()
-		self._perc_down(1)
-
-		return element
-
+        return element
