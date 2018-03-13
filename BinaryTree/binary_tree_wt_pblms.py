d020672920aec5bc3d73e12370007e9b52dead0e
Sriram Ganesh <sriram@belong.co>
2018-03-13 21:11:31 +0530

    Creating LRUCache

--- BinaryTree/binary_tree_wt_pblms.py
+++ BinaryTree/binary_tree_wt_pblms.py
@@ -33,18 +33,59 @@ class Node(object):
         self.right = right
 
 
-class BinaryTree(object):
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        pass
-
-    def delete(self, data):
-        pass
-
-
 class BinaryTreeUtil(object):
+    def insert(self, root, data):
+
+        if root.data < data:
+
+            if root.right is None:
+                root.right = Node(data)
+
+            else:
+                self.insert(root.right, data)
+        else:
+
+            if root.left is None:
+                root.left = Node(data)
+
+            else:
+                self.insert(root.left, data)
+
+    def max_val_node(self, root):
+        current = root
+
+        while current.right is not None:
+            current = current.right
+
+        return current
+
+    def delete(self, root, data):
+        if root is None:
+            return
+
+        elif root.data < data:
+            root.right = self.delete(root.right, data)
+
+        elif root.data > data:
+            root.left = self.delete(root.left, data)
+
+        else:
+
+            if root.left is None:
+                temp = root.right
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                return temp
+
+            else:
+                _max_val_node = self.max_val_node(root.left)
+                root.data = _max_val_node.data
+                self.delete(root.left, _max_val_node.data)
+
+        return root
+
     def in_order(self, root):
         """
         l => ro => ri
@@ -63,6 +104,20 @@ class BinaryTreeUtil(object):
 
     def post_order(self, root):
         pass
+
+    def findkthlargest(self, root, k, n):
+        if not root:
+            return
+
+        self.findkthlargest(root.right, k, n)
+
+        if n == k:
+            print "Lar: %s" % root.data
+            return
+
+        n += 1
+
+        self.findkthlargest(root.left, k, n)
 
     def vertical_sum(self, root):
 
@@ -97,25 +152,125 @@ class BinaryTreeUtil(object):
     def max_width(self, root):
         pass
 
+    def sum_of_leaf_nodes(self, root):
+
+        queue = [root]
+        sum = 0
+
+        while len(queue):
+
+            node = queue.pop()
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+            if not node.left and not node.right:
+                sum += node.data
+
+        return sum
+
+    def max_height(self, root):
+
+        queue = [root]
+        height = 0
+
+        while len(queue):
+
+            node = queue.pop()
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+            if node.right or node.left:
+                height += 1
+
+        return height
+
+    def print_all_path(self, root, nodes):
+        if not root:
+            return
+
+        if root.left is None and root.right is None:
+            print ' -> '.join(map(str, nodes + [root.data]))
+            return
+
+        if root.left:
+            self.print_all_path(root.left, nodes + [root.data])
+
+        if root.right:
+            self.print_all_path(root.right, nodes + [root.data])
+
+    def print_ancestors(self, root, k):
+        if not root:
+            return
+
+        if root.data == k:
+            return True
+
+        if self.print_ancestors(root.left, k) or self.print_ancestors(root.right, k):
+            print root.data
+            return True
+
+    def size(self, root, size=0):
+
+        if not root:
+            return 0
+
+        return self.size(root.left, size+1) + 1 + self.size(root.right, size+1)
+
+    def hasPathSum(self, root, sum):
+
+        subsum = sum - root.data
+
+        if not root:
+            return sum
+
+        if root.left:
+            s = self.hasPathSum(root.left, subsum)
+
+        if root.right:
+            s = self.hasPathSum(root.left, subsum)
+
+        return s
 
 if __name__ == '__main__':
     # Driver
 
     root = Node(5)
-
     root.left = Node(2)
-    root.right = Node(4)
+    root.right = Node(3)
+    root.left.left = Node(6)
+    root.left.left.right = Node(10)
+    root.left.right = Node(7)
+    root.right.left = Node(8)
+    root.right.right = Node(9)
 
-    root.left.left = Node(1)
-    root.left.right = Node(3)
-
-    root.right.left = Node(6)
-    root.right.right = Node(8)
-
+    # elements = [1, 3, 4, 6, 8]
     bu = BinaryTreeUtil()
 
+    # for each in elements:
+    #     bu.insert(root, each)
+
     # bu.in_order(root)
-    print(bu.vertical_sum(root))
+    print '\n'
+    # bu.findkthlargest(root, 0, 0)
+    # print bu.sum_of_leaf_nodes(root)
+    # bu.delete(root, 5)
+    # h = bu.max_height(root)
+    # print
+    # print h
+    # bu.print_all_path(root, [])
+    # bu.print_ancestors(root, 10)
+    # print 'Size: ', bu.size(root)
+    print 'Size: ', bu.hasPathSum(root, 16)
+    # bu.in_order(root)
+    # print(bu.vertical_sum(root))
 
 
 """
---
+++ Queue/queue.py
@@ -0,0 +1,134 @@
+class QueueUsingStack(object):
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, element):
+        self.stack1.append(element)
+
+    def dequeue(self):
+        if not self.stack1:
+            print('Queue is empty')
+            return
+
+        self.stack2 = self.stack1[::-1]
+        res = self.stack2.pop()
+        self.stack1 = self.stack2[::-1]
+
+        return res
+
+    def head(self):
+        return self.stack1[0] if self.stack1 else None
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue(object):
+    def __init__(self, capacity):
+        self.front = None
+        self.rear = None
+        self.capacity = capacity
+
+    def enQueue(self, data):
+
+        if self.isFull():
+            raise Exception('Queue is Full')
+
+        if not self.rear:
+            self.rear = Node(data)
+            self.front = self.rear
+
+        else:
+            last = self.rear
+            last.next = Node(data)
+            self.rear = last.next
+
+    def deQueue(self):
+        if not self.front:
+            raise Exception("Queue is Empty")
+
+        first = self.front
+        self.front = first.next
+
+        return first.data
+
+    def isEmpty(self):
+        return bool(self.front or self.rear)
+
+    def size(self, front, count=0):
+        if not front:
+            return count
+
+        return self.size(front.next, count+1)
+
+    def isFull(self):
+        return self.size(self.front) >= self.capacity
+
+    def printQueue(self, front):
+        if not front:
+            return
+
+        print front.data
+        self.printQueue(front.next)
+
+
+class QueueUsingDLL(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    # q = QueueUsingStack()
+    #
+    # t = int(raw_input())
+    #
+    # for a0 in range(0, t):
+    #     input = map(int, raw_input().split(' '))
+    #
+    #     if input[0] == 1:
+    #         # EnQ
+    #         q.enqueue(input[1])
+    #
+    #     elif input[0] == 2:
+    #         # DeQ
+    #         q.dequeue()
+    #
+    #     elif input[0] == 3:
+    #         # print
+    #         print(q.head())
+
+    q = Queue(10)
+
+    elements = [1, 2, 3, 4, 5, 8]
+
+    for each in elements:
+        q.enQueue(each)
+
+    print 'Size: ', q.size(q.front)
+    print 'Elements: '
+    q.printQueue(q.front)
+
+    print 'DeQueue: 1'
+    print q.deQueue()
+    print 'DeQueue: 2'
+    print q.deQueue()
+    # print 'DeQueue: 3'
+    # print q.deQueue()
+    # print 'DeQueue: 4'
+    # print q.deQueue()
+    # print 'DeQueue: 5'
+    # print q.deQueue()
+    # print 'DeQueue: 6'
+    # print q.deQueue()
+    # print 'DeQueue: 7'
+    # print q.deQueue()
+
+    print 'Elements: '
+    q.printQueue(q.front)
+
+
+main()
